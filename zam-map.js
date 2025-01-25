b1e91e4ea7cf6d157bc4b1d6750ac7a68857983c
Matteo Forlani <matteo@matthew5pl.net>
2025-01-25 17:37:15 +0100

    prova con leaflet

--- zam-map.js
+++ zam-map.js
@@ -1,9 +1,67 @@
-const ZAMMapType = {
+const ZAMMapFloor = {
     Floor1: "floor-01"
 };
 
+function _zamclamp(v, min, max) {
+    return Math.min(Math.max(v, min), max);
+};
+
+class ZAMMarker {
+    constructor(id, x, y) {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+
+        this.element = document.createElement("div");
+        this.element.id = "zam-marker-" + id;
+        this.element.className = "zam-map-marker";
+
+        this.element.style.top = y + "px";
+        this.element.style.left = x + "px";
+
+        this.element.style.scale = "100%";
+    }
+
+    getElement() {
+        return this.element;
+    }
+
+    render(offsetX, offsetY) {
+        var currentX = parseFloat(this.element.style.left, 10);
+        var currentY = parseFloat(this.element.style.top, 10);
+
+        this.element.style.top = currentY + offsetY + "px";
+        this.element.style.left = currentX + offsetX + "px";
+    }
+
+    zoom(e, minz, maxz) {
+        var currentZoom = parseFloat(this.element.style.scale, 10);
+        var newZoom = _zamclamp(currentZoom + e.deltaY, minz, maxz);
+
+        var zoomFactor = newZoom / currentZoom;
+
+        var rect = this.element.getBoundingClientRect();
+        
+        var mouseX = e.clientX - rect.left; // Mouse X relative to element
+        var mouseY = e.clientY - rect.top;  // Mouse Y relative to element
+        
+        var currentX = parseFloat(this.element.style.left, 10);
+        var currentY = parseFloat(this.element.style.top, 10);
+        
+        var offsetX = (mouseX - currentX) * (zoomFactor - 1);
+        var offsetY = (mouseY - currentY) * (zoomFactor - 1);
+        
+        this.element.style.left = (currentX - offsetX) + "px";
+        this.element.style.top = (currentY - offsetY) + "px";
+        
+        this.element.style.scale = newZoom + "%";
+
+        console.log(newZoom);
+    }
+}
+
 class ZAMMap {
-    constructor(/** @type {ZAMMapType} */ type, w, h, minz, maxz) {
+    constructor(/** @type {ZAMMapFloor} */ type, w, h, minz, maxz) {
         this.map = document.createElement("div");
         this.map.className = "zam-map";
 
@@ -21,9 +79,7 @@ class ZAMMap {
 
         this.mouseDown = false;
 
-        var clamp = (v, min, max) => {
-            return Math.min(Math.max(v, min), max);
-        };
+        this.markers = [];
 
         this.map.ondragstart = () => {
             return false;
@@ -44,6 +100,10 @@ class ZAMMap {
 
                 this.map.style.backgroundPositionY = currentY + e.movementY + "px";
                 this.map.style.backgroundPositionX = currentX + e.movementX + "px";
+
+                this.markers.forEach((/** @type {ZAMMarker} */ marker) => {
+                    marker.render(e.movementX, e.movementY);
+                });
             }
         };
 
@@ -51,9 +111,13 @@ class ZAMMap {
             e.preventDefault();
 
             var currentZoom = parseFloat(this.map.style.backgroundSize, 10);
-            var newZoom = clamp(currentZoom + e.deltaY, minz, maxz);
+            var newZoom = _zamclamp(currentZoom + e.deltaY, minz, maxz);
 
             var zoomFactor = newZoom / currentZoom;
+
+            this.markers.forEach((/** @type {ZAMMarker} */ marker) => {
+                //marker.zoom(e, minz, maxz);
+            });
 
             var rect = this.map.getBoundingClientRect();
             var mouseX = e.clientX - rect.left; // Mouse X relative to map
@@ -70,5 +134,10 @@ class ZAMMap {
 
             this.map.style.backgroundSize = newZoom + "%";
         };
+    }
+
+    addMarker(/** @type {ZAMMarker} */ marker) {
+        this.markers.push(marker);
+        this.map.appendChild(marker.getElement());
     }
 }
